60f2910112415f4165c88f7513e46c86b9665221
emilytwines <emdelbridge@gmail.com>
2022-12-14 16:02:48 -0500

    created Components and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import './App.css';
+import './Components/CardContainer/AddCard.jsx'
+import './Components/CardContainer/Card.jsx'
+import './Components/CardContainer/CardContainer.jsx'
+import './Components/CardContainer/CardViewer.jsx'
+import './Components/CardContainer/DeleteCard.jsx'
+import './Components/CardContainer/EditCard.jsx'
+import './Components/CardContainer/NextCard.jsx'
+import './Components/CollectionContainer/Collection.jsx'
+import './Components/CollectionContainer/CollectionContainer.jsx'
+import './Components/Header/Header.jsx'
+import './Components/Sidebar/Sidebar.jsx'
+
+import "bootstrap/dist/css/bootstrap.css";
+import React, { useState, useEffect } from "react";
+
+
+
+
+
+
+
 
 function App() {
   return (
---
+++ src/Components/CardContainer/NextCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NextCard() {
+  return (
+    <div>NextCard</div>
+  )
+}
