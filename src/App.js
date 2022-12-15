c7664ce3852396cd8a4fdf89efe792d1ee2fe820
emilytwines <emdelbridge@gmail.com>
2022-12-15 10:42:44 -0500

    fixed sidebar fomatting, beginning functional coding

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ import Collection from './Components/Sidebar/CollectionContainer/Collection.jsx'
 import CollectionsContainer from './Components/Sidebar/CollectionContainer/CollectionContainer.jsx';
 import Header from './Components/Header/Header.jsx';
 import Sidebar from './Components/Sidebar/Sidebar.jsx';
+import FontFace from './Components/FontFace/FontFace';
 
 import "bootstrap/dist/css/bootstrap.css";
+import './Components/FontFace/Fonts/Freedom.ttf'
+import './index.css';
+
+
 import React, { useState, useEffect } from "react";
 
 
@@ -23,10 +28,12 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div className="App">
+
+    <div className = "App">
+      <div className  ='sidebar-container'>
         <Header/>
         <Sidebar/>
-
+      </div>
     </div>
   );
 }
---
+++ src/Components/FontFace/FontFace.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FontFace() {
+  return (
+    <div>
+        <div className="card">
+        <div className="font-face-gm">
+
+    </div>
+    </div>
+    </div>
+  )
+}
